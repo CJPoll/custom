491d8cc7387aba3d923d99bbf607ab6fe11852f0
Cody J. Poll <cjpoll@gmail.com>
2017-08-28 09:36:15 -0600

    Update

--- scripts/main_pane_height.sh
+++ scripts/main_pane_height.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CLIENT_HEIGHT=$(tmux display-message -p '#{client_height}');
-MINOR_PANE_HEIGHT=$(((${CLIENT_HEIGHT} / 4)));
+MINOR_PANE_HEIGHT=$(((${CLIENT_HEIGHT} / 3)));
 MAIN_PANE_HEIGHT=$((($CLIENT_HEIGHT - $MINOR_PANE_HEIGHT)));
 
 tmux set-window-option -g main-pane-height ${MAIN_PANE_HEIGHT};
